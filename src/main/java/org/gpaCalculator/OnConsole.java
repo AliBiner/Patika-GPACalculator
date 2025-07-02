fdbd944a56c1eaea8ed7f341be5331f059679672
Ali Biner <alibiner.github@gmail.com>
2025-07-02 17:14:52 +0300

    Control of console input type

--- src/main/java/org/gpaCalculator/OnConsole.java
+++ src/main/java/org/gpaCalculator/OnConsole.java
@@ -1,33 +1,59 @@
 package org.gpaCalculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class OnConsole {
 
-    public static int getIntValueOnConsole(char valueName){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(valueName + " sayısını giriniz: ");
-        return scanner.nextInt();
+    public static int getIntValueOnConsole(char valueName, String text){
+        int value;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(valueName + " " + text);
+            value = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! Lütfen tam sayı giriniz.");
+            value =getIntValueOnConsole(valueName,text);
+        }
+        return value;
     }
 
-    public static int getIntValueOnConsole(String valueName){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(valueName + " sayısını giriniz: ");
-        return scanner.nextInt();
+    public static int getIntValueOnConsole(String valueName, String text) {
+        int value;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(valueName + " " + text);
+            value = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! Lütfen tam sayı giriniz.");
+            value =getIntValueOnConsole(valueName,text);
+        }
+        return value;
     }
 
-    public static Double getDoubleValueOnConsole(char valueName){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(valueName + " sayısını giriniz: ");
-        return scanner.nextDouble();
+    public static double getDoubleValueOnConsole(char valueName, String text){
+        double value;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(valueName + " " + text);
+            value = scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! Lütfen ondalıklı sayı giriniz.");
+            value =getIntValueOnConsole(valueName,text);
+        }
+        return value;
     }
 
-    public static Double getDoubleValueOnConsole(String valueName){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(valueName + " sayısını giriniz: ");
-        return scanner.nextDouble();
+    public static double getDoubleValueOnConsole(String valueName, String text){
+        double value;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(valueName + " " + text);
+            value = scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı giriş! Lütfen ondalıklı sayı giriniz.");
+            value =getIntValueOnConsole(valueName,text);
+        }
+        return value;
     }
-
-
-
 }
